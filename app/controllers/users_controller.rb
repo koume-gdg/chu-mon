20186b9935f3a383dfc74350a1893ad09d74fd40
koume-gdg <gdgpll.vmj@gmail.com>
2021-02-13 16:16:02 +0900

    user一覧表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+  #before_action :set_user, only: [:index]
   def index
+    @users = User.all.order('created_at DESC')
+    
   end
-  
+
+  private
+
+
+  #def set_user
+    #@user = User.find(params[:id])
+  #end
+
 end
