23859e832e63be111e8feba499f06329662b5a79
koume-gdg <gdgpll.vmj@gmail.com>
2021-02-14 17:26:16 +0900

    orderコントローラー記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,19 @@
 class OrdersController < ApplicationController
+  before_action :set_menu
+
+  
+  def index
+    @order_seat = OrderSeat.new
+  end
+
+  def create
+  end
+
+  private
+
+  def set_menu
+    @menu = Menu.find(params[:menu_id])
+  end
+
+
 end
