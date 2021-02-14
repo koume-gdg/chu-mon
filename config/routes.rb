788e27e6ae85464c8514a9ea65bf78d838299b9e
koume-gdg <gdgpll.vmj@gmail.com>
2021-02-14 17:40:11 +0900

    購入画面へ遷移

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to:'users#index'
   resources :menus do
-    resources :order, only: [:index, :create]
+    resources :orders, only: [:index, :create]
+  end
+
   resources :users
 end
